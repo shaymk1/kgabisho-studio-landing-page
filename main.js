32e9c8a398e382df59a7a192a7cd3739a8af2857
unknown <mkekae@gmail.com>
2023-03-14 17:02:13 +0200

    fixing some accessibility issues

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ hamburger.addEventListener("click", () => {
 	navMenu.classList.toggle("active");
 });
 
-// document.querySelectorAll(".nav-link").forEach((n) =>
-// 	n.addEventListener("click", () => {
-// 		hamburger.classList.remove("active");
-// 		navMenu.classList.remove("active");
-// 	})
-// );
+document.querySelectorAll(".nav-link").forEach((n) =>
+	n.addEventListener("click", () => {
+		hamburger.classList.remove("active");
+		navMenu.classList.remove("active");
+	})
+);
